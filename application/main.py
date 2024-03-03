7f72a216ddbf5f6cace4fdf973b1e85e0d818488
Meher Vamsi Dontoju <75484855+Mehervamsi1@users.noreply.github.com>
2024-03-03 10:53:56 +0000

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch dev-1.0.0
     Your branch is up to date with 'origin/dev-1.0.0'.
    
     Changes to be committed:
    	modified:   application/main.py
    	modified:   application/templates/login.html
    	new file:   application/templates/register.html
    	new file:   application/venv/lib/python3.10/site-packages/__pycache__/antiorm.cpython-310.pyc
    	new file:   application/venv/lib/python3.10/site-packages/__pycache__/antipool.cpython-310.pyc
    	new file:   application/venv/lib/python3.10/site-packages/__pycache__/db_sqlite3.cpython-310.pyc
    	new file:   application/venv/lib/python3.10/site-packages/__pycache__/dbapiext.cpython-310.pyc
    	new file:   application/venv/lib/python3.10/site-packages/__pycache__/dbrelmgr.cpython-310.pyc
    	new file:   application/venv/lib/python3.10/site-packages/antiorm-1.2.1-py3.10.egg-info/PKG-INFO
    	new file:   application/venv/lib/python3.10/site-packages/antiorm-1.2.1-py3.10.egg-info/SOURCES.txt
    	new file:   application/venv/lib/python3.10/site-packages/antiorm-1.2.1-py3.10.egg-info/dependency_links.txt
    	new file:   application/venv/lib/python3.10/site-packages/antiorm-1.2.1-py3.10.egg-info/installed-files.txt
    	new file:   application/venv/lib/python3.10/site-packages/antiorm-1.2.1-py3.10.egg-info/top_level.txt
    	new file:   application/venv/lib/python3.10/site-packages/antiorm.py
    	new file:   application/venv/lib/python3.10/site-packages/antipool.py
    	new file:   application/venv/lib/python3.10/site-packages/db-0.1.1-py3.10.egg-info/PKG-INFO
    	new file:   application/venv/lib/python3.10/site-packages/db-0.1.1-py3.10.egg-info/SOURCES.txt
    	new file:   application/venv/lib/python3.10/site-packages/db-0.1.1-py3.10.egg-info/dependency_links.txt
    	new file:   application/venv/lib/python3.10/site-packages/db-0.1.1-py3.10.egg-info/installed-files.txt
    	new file:   application/venv/lib/python3.10/site-packages/db-0.1.1-py3.10.egg-info/requires.txt
    	new file:   application/venv/lib/python3.10/site-packages/db-0.1.1-py3.10.egg-info/top_level.txt
    	new file:   application/venv/lib/python3.10/site-packages/db/__init__.py
    	new file:   application/venv/lib/python3.10/site-packages/db/__pycache__/drivers.cpython-310.pyc
    	new file:   application/venv/lib/python3.10/site-packages/db/drivers.py
    	new file:   application/venv/lib/python3.10/site-packages/db_sqlite3-0.0.1-py3.10.egg-info/PKG-INFO
    	new file:   application/venv/lib/python3.10/site-packages/db_sqlite3-0.0.1-py3.10.egg-info/SOURCES.txt
    	new file:   application/venv/lib/python3.10/site-packages/db_sqlite3-0.0.1-py3.10.egg-info/dependency_links.txt
    	new file:   application/venv/lib/python3.10/site-packages/db_sqlite3-0.0.1-py3.10.egg-info/installed-files.txt
    	new file:   application/venv/lib/python3.10/site-packages/db_sqlite3-0.0.1-py3.10.egg-info/requires.txt
    	new file:   application/venv/lib/python3.10/site-packages/db_sqlite3-0.0.1-py3.10.egg-info/top_level.txt
    	new file:   application/venv/lib/python3.10/site-packages/db_sqlite3.py
    	new file:   application/venv/lib/python3.10/site-packages/dbapiext.py
    	new file:   application/venv/lib/python3.10/site-packages/dbrelmgr.py

--- application/main.py
+++ application/main.py
@@ -2,8 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def login():
     return render_template('login.html')
+
+@app.route('/registerpage')
+def register():
+    return render_template('register.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
